76e8fae8ad5a660e793061b02a633cf6914d6265
Michael Stearns <49793053+MichaelBStearns@users.noreply.github.com>
2022-04-12 09:55:12 -0400

    Add files via upload

---
+++ ROS/Swarm/build/CTestTestfile.cmake
@@ -0,0 +1,13 @@
+# CMake generated Testfile for 
+# Source directory: /home/mbs4433/Swarm/src
+# Build directory: /home/mbs4433/Swarm/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("MABL/amigo_real")
+subdirs("MABL/amigo_sim")
+subdirs("swarm_msgs")
+subdirs("hardware")
+subdirs("MABL/amigo_description")
+subdirs("aco_ros")
